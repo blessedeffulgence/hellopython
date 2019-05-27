0834a7dc2763ed89cd1a0ac17d533be7715a792d
bless <blessedeffulgence@gmail.com>
2019-05-27 20:36:32 +0300

    Initial commit

---
+++ character.py
@@ -0,0 +1,6 @@
+character_name = "John"
+character_age = "70"
+print("There once lived a man named"  + character_name + ".")
+print("He was"  + character_age + "years old.")
+print("He liked being called"  + character_name + ",")
+print("but didn't like being"  + character_age + "years")
---
+++ password validation.py
@@ -0,0 +1,8 @@
+password=input("Enter a password:")
+if len(password)>6 and len(password)<12:
+    print("Password is valid")
+
+else:
+      print("Password not valid")
+raw_input=a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+text_input= raw_input  ('Type anything:')
